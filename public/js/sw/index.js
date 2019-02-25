838b52562787e804d5b68d0f7edd824556ac4d87
m0r00gq <mrico@walmartlabs.com>
2019-02-24 20:35:05 -0800

    Boilerplate for responding task install exc

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -14,6 +14,10 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
+  // get event url to run a match
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
