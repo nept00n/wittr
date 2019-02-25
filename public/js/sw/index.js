ae207fccfe8e18511b0bd5c252e4792ce9e38236
m0r00gq <mrico@walmartlabs.com>
2019-02-24 20:30:13 -0800

    Intercept images

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,10 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  var request = event.request;
+  if (/\.jpg$/.test(request.url)) {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
+  }
 });
