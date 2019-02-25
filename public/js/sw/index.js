4b7f1ff4fc908a184154464c37a5c11d75c67424
m0r00gq <mrico@walmartlabs.com>
2019-02-24 20:16:35 -0800

    Complete task-custom-response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,12 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  const strong = '<strong class="a-winner-is-me">Hiya partner</strong>';
+  event.respondWith(
+    new Response(strong, {
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8'
+      }
+    })
+  );
 });
